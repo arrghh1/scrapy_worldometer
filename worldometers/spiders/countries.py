7a265a209f63ace5dc508eee1cb9162f56f73d8b
arrghh1 <rowancguard-misc@yahoo.com.au>
2020-04-12 23:18:25 +1000

    added country name via meta between parse functions

--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -16,14 +16,16 @@ class CountriesSpider(scrapy.Spider):
 
         #secondly, we want to make sure we pass an object back (ticker?)
                 #absolute_url = response.urljoin(link) # this works becaquse it knows the domain already. f"https://www.worldometers.info{link}"
-                yield response.follow(url=link, callback=self.parse_country)
+                yield response.follow(url=link, callback=self.parse_country, meta={'country_name':name})
 
     def parse_country(self, response):
+        name = response.request.meta['country_name'] #gets the meta from the other parse function
         rows = response.xpath(f"(//table[contains(@class,'table-list')])[1]/tbody/tr")
         for row in rows:
             year = row.xpath(".//td[1]/text()").get()
             population = row.xpath(".//td[2]/strong/text()").get()
             yield {
+                'country_name':name,
                 'year':year,
                 'population':population
             }
