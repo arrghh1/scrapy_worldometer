754c820e49169ac88bb3682005be81e01b095fe5
arrghh1 <rowancguard-misc@yahoo.com.au>
2020-04-12 22:34:27 +1000

    initial commit

---
+++ worldometers/spiders/countries.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class CountriesSpider(scrapy.Spider):
+    name = 'countries'
+    allowed_domains = ['www.worldometers.info']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response):
+        #firstly, remove the pass, as we're going to send stuff back. Use the xpaths we saw earlier by investigating the site
+        countries = response.xpath("//td/a")
+        for country in countries:
+                name = country.xpath(".//text()").get() #selected object, and not response object, so requires a period at the start
+                link = country.xpath(".//@href").get()
+
+        #secondly, we want to make sure we pass an object back (ticker?)
+                yield {
+                    'country_name':name,
+                    'country_link':link
+                }
